3f070c4153add8b4e920c712a3f961d52ecd6fc5
Abed Tawil <aboditawil@gmail.com>
2022-04-14 19:09:37 +0300

    add validation to push notifiactions repo

--- repository/PushNotifications.js
+++ repository/PushNotifications.js
@@ -1,14 +1,36 @@
 export default class PushNotifications {
     static schedule = (options) => {
         var _options = {
-            title: options.title || '',
-            text: options.text || '',
+            title: options.title || null,
+            text: options.text || null,
+            inAppMessage: options.inAppMessage || null,
             at: options.at || new Date(),
+            users: options.users || null,
+            userTags: options.userTags || null,
             groupName: options.groupName || null,
+            queryString: options.queryString || null,
+            sendToSelf: options.sendToSelf || true,
         };
+
+        if (!_options.title || !_options.text) {
+            return console.error('Required options are missing.');
+        }
 
         return new Promise((resolve, reject) => {
             buildfire.notifications.pushNotification.schedule(_options, (err, res) => {
+                if (err) return reject(err);
+                resolve(res);
+            });
+        });
+    }
+
+    static cancel = (notificationId) => {
+        if (!notificationId) {
+            return console.error('Required options are missing.');
+        }
+
+        return new Promise((resolve, reject) => {
+            buildfire.notifications.pushNotification.cancel(notificationId, (err, res) => {
                 if (err) return reject(err);
                 resolve(res);
             });
@@ -32,6 +54,10 @@ export default class PushNotifications {
         var _options = {
             groupName: options.groupName || null,
         };
+
+        if (!_options.groupName) {
+            return console.error('Required options are missing.');
+        }
 
         return new Promise((resolve, reject) => {
             buildfire.notifications.pushNotification.unsubscribe(_options, (err, res) => {

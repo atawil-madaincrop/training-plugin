ed72c277e99096f3da51ffeb850b7a5b4188f2c7
Abed Tawil <aboditawil@gmail.com>
2022-04-14 19:09:56 +0300

    add validation to analytics repo

--- repository/Analytics.js
+++ repository/Analytics.js
@@ -1,21 +1,33 @@
 export default class Analytics {
     static trackView = (eventName, metaData) => {
+        if (!eventName) {
+            return console.error('Required options are missing.');
+        }
+
         buildfire.analytics.trackView(eventName, metaData);
     }
 
     static trackAction = (eventName, metaData) => {
+        if (!eventName) {
+            return console.error('Required options are missing.');
+        }
+
         buildfire.analytics.trackAction(eventName, metaData);
     }
 
     static registerEvent = (event, options) => {
         var _event = {
-            key: event.key || undefined,
             title: event.title || null,
+            key: event.key || undefined,
             description: event.description || null,
         };
 
         var _options = {
             silentNotification: options.silentNotification || true,
+        }
+
+        if (!_event.title || !_event.key) {
+            return console.error('Required options are missing.');
         }
 
         return new Promise((resolve, reject) => {
@@ -27,6 +39,10 @@ export default class Analytics {
     }
 
     static unregisterEvent = (key) => {
+        if (!key) {
+            return console.error('Required options are missing.');
+        }
+
         return new Promise((resolve, reject) => {
             buildfire.notifications.pushNotification.unregisterEvent(key, (err, res) => {
                 if (err) return reject(err);
@@ -38,6 +54,10 @@ export default class Analytics {
     static showReports = (options) => {
         var _options = {
             eventKey: options.eventKey || undefined,
+        }
+
+        if (!_event.eventKey) {
+            return console.error('Required options are missing.');
         }
 
         return new Promise((resolve, reject) => {

d6f95b42864c595e534af8ad80afbeaa659d65a4
Abed Tawil <aboditawil@gmail.com>
2022-04-14 17:13:00 +0300

    remove test code from widget js

--- widget/widget.js
+++ widget/widget.js
@@ -1,10 +1,5 @@
-import Items from "../repository/Items.js";
-
 const init = async () => {
-    var items = await Items.search();
-    var item = await Items.find("62573e4508c3a90378bc6e92");
-    console.log('items:', items);
-    console.log('item:', item);
+
 }
 
 init();

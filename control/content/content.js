3bb5f8b834b8119caa2d5aca916891d878cd9e15
Abed Tawil <aboditawil@gmail.com>
2022-04-19 19:22:45 +0300

    add search functionality to items table

--- control/content/content.js
+++ control/content/content.js
@@ -1,8 +1,22 @@
 import Items from "../../widget/common/repository/Items.js";
 
+let search;
+const searchInput = document.getElementById("search");
+const searchButton = document.getElementById("search-button");
+
 const initItemsTable = () => {
     let searchTableHelper = new SearchTableHelper("itemsTable", Items.TAG, searchTableConfig);
     searchTableHelper.search();
+
+    searchButton.onclick = () => {
+        search = searchInput.value;
+        searchTableHelper.search({
+            $or: [
+                { "$json.title": { "$regex": search, "$options": "i" } },
+                { "$json.subtitle": { "$regex": search, "$options": "i" } },
+            ],
+        });
+    }
 }
 
 const init = async () => {

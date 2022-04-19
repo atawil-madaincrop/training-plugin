1dc569b346517a517d940a2f25ef756856595b8c
Abed Tawil <aboditawil@gmail.com>
2022-04-19 15:13:04 +0300

    remove unused files from content and introduction

--- control/content/content.js
+++ control/content/content.js
@@ -1,16 +1,4 @@
-const initTiny = (selector) => {
-    tinymce.init({
-        selector: selector,
-    });
-}
-
-const initCarousel = (selector) => {
-    let editor = new buildfire.components.carousel.editor(selector, []);
-}
-
 const init = async () => {
-    initTiny("#wysiwygContent");
-    initCarousel(".carousel");
 }
 
 init();

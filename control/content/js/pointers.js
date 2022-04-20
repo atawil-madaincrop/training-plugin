ec3067d39a77ae5b4eeacff09e74660de404743f
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-20 22:09:19 +0300

    put all getting elements from HTML files to be pointer from this file

---
+++ control/content/js/pointers.js
@@ -0,0 +1,19 @@
+
+
+export const pointers = {
+    addItemBtn: document.getElementById("add-New-Item_btn"),
+    cancelAdding: document.getElementById("cancel-Adding-Process"),
+    addItemContainer: document.getElementById("add-New-Item-To-DataStore"),
+    title: document.getElementById("title"),
+    subTitle: document.getElementById("subTitle"),
+    getSearch: document.getElementById("getSearch"),
+    iconPlcaeCancelSearch: document.getElementById("iconPlcaeCancelSearch"),
+    formPage: document.getElementById("formPage"),
+    getSearchInput: document.getElementById("getSearchInput"),
+    iconPlcae: document.getElementById("iconPlcaeSearch"),
+    printOutTableContainer: document.getElementById("printOutTable"),
+    itemsPageDiv: document.getElementById("list-Of-Items-From-DataStore"),
+    sortSpan: document.getElementById("sortSpan"),
+}
+
+

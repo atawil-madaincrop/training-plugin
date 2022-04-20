9f5a97baa68332b38a7cb16ce605b1a61b7eaf0c
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-20 17:44:09 +0300

    manage submit form in a beter way

--- control/content/js/eventHandlers.js
+++ control/content/js/eventHandlers.js
@@ -2,8 +2,6 @@ import {ShowControler} from "./showControler.js";
 import { ContentHandlers } from "./contentHandlers.js";
 
 let formPage = document.getElementById("formPage");
-let title = document.getElementById("title");
-let subTitle = document.getElementById("subTitle");
 let getSearch = document.getElementById("getSearch");
 let getSearchInput = document.getElementById("getSearchInput");
 let iconPlcae = document.getElementById("iconPlcaeSearch");
@@ -13,21 +11,38 @@ iconPlcaeCancelSearch.style.display = "none";
 
 export class EventHandlers {
 
+    // get and print Items in the list
+    static loadItems = async () => {
+        formPage.style.display = "none";
+        let itemsData = await ContentHandlers.loadItems(0, 10);
+        ShowControler.mySateArr = itemsData;
+    }
+    static getSearchItems = async () => {
+        if (getSearchInput.value.length > 0) {
+            ShowControler.loading();
+            let res = await ContentHandlers.searchItems(getSearchInput.value);
+            ShowControler.mySateArr = res;
+            iconPlcae.style.display = "none";
+            iconPlcaeCancelSearch.style.display = "inline-block";
+        } else {
+            EventHandlers.loadItems();
+            EventHandlers.setAddBtn()
+        }
+        ShowControler.printItems(ShowControler.mySateArr);
+    }
+    static resetSearch() {
+        ShowControler.loading();
+        getSearchInput.value = "";
+        iconPlcaeCancelSearch.style.display = "none";
+        iconPlcae.style.display = "inline-block";
+        EventHandlers.loadItems();
+        ShowControler.printItems(ShowControler.mySateArr);
+        EventHandlers.setAddBtn();
+    }
+    // handel input data
     static changeIcon(e) {
         iconPlcae.className = "icon icon-magnifier";
         getSearch.addEventListener('click', EventHandlers.getSearchItems)
-    }
-    static setAddBtn() {
-        let newAdd = document.getElementById("add-New-Item_btn2");
-        if (newAdd) {
-            newAdd.addEventListener("click", () => ShowControler.showAddModal(true))
-        }
-    }
-    static emptyData() {
-        title.value = null;
-        subTitle.value = null;
-        ShowControler.image.clear();
-        ShowControler.coverImage.clear();
     }
     static handelTitle(e) {
         ShowControler.newItem.title = e.target.value;
@@ -38,10 +53,34 @@ export class EventHandlers {
     static handelTiny() {
         ShowControler.newItem.description = tinymce.activeEditor.getContent();
     }
-    static loadItems = async () => {
-        formPage.style.display = "none";
-        let itemsData = await ContentHandlers.loadItems(0, 10);
-        ShowControler.mySateArr = itemsData;
+    // handel form to add and edit items 
+    static setAddBtn() {
+        let newAdd = document.getElementById("add-New-Item_btn2");
+        if (newAdd) {
+            newAdd.addEventListener("click", () => ShowControler.showAddModal(true))
+        }
+    }
+    static submitNewItem = async () => {
+        if (ShowControler.newItem.image && ShowControler.newItem.coverImage) {
+            await EventHandlers.handelSubmitForm();
+        } else {
+            console.log("----- Required Data missing -----");
+        }
+    }
+    static handelSubmitForm = async() => {
+        if(ShowControler.typeOfHandelForm == "add"){
+            let addedData = await ContentHandlers.addItem(ShowControler.newItem)
+            ShowControler.mySateArr.splice(0, 0, addedData)
+            ShowControler.emptyData();
+            ShowControler.showAddModal(false);
+            ShowControler.printItems(ShowControler.mySateArr);
+        }else if(ShowControler.typeOfHandelForm == "edit"){
+            let editedData = await ContentHandlers.editItem(ShowControler.itemForEdit.itemElement.id,ShowControler.newItem);
+            ShowControler.mySateArr.splice(ShowControler.itemForEdit.index, 1, editedData);
+            ShowControler.emptyData();
+            ShowControler.showAddModal(false);
+            ShowControler.printItems(ShowControler.mySateArr);
+        }
     }
     static pushNewRow(item) {
         if (ShowControler.mySateArr.length == 1) {
@@ -70,40 +109,5 @@ export class EventHandlers {
             let deleteBtn = document.getElementById(`deleteItemBtn-${ShowControler.mySateArr.length - 1}`);
             deleteBtn.addEventListener("click", () => deleteRow(newRow, item))
         }
-    }
-    static submitNewItem = async () => {
-        if (ShowControler.newItem.image && ShowControler.newItem.coverImage) {
-            let addedData = await ContentHandlers.addItem(ShowControler.newItem)
-            // ShowControler.mySateArr.push(addedData);
-            ShowControler.mySateArr.splice(0, 0, addedData)
-            EventHandlers.emptyData();
-            ShowControler.showAddModal(false);
-            ShowControler.printItems(ShowControler.mySateArr);
-            // pushNewRow(addedData);
-        } else {
-            console.log("----- Required Data missing -----");
-        }
-    }
-    static getSearchItems = async () => {
-        if (getSearchInput.value.length > 0) {
-            ShowControler.loading();
-            let res = await ContentHandlers.searchItems(getSearchInput.value);
-            ShowControler.mySateArr = res;
-            iconPlcae.style.display = "none";
-            iconPlcaeCancelSearch.style.display = "inline-block";
-        } else {
-            EventHandlers.loadItems();
-            EventHandlers.setAddBtn()
-        }
-        ShowControler.printItems(ShowControler.mySateArr);
-    }
-    static resetSearch() {
-        ShowControler.loading();
-        getSearchInput.value = "";
-        iconPlcaeCancelSearch.style.display = "none";
-        iconPlcae.style.display = "inline-block";
-        EventHandlers.loadItems();
-        ShowControler.printItems(ShowControler.mySateArr);
-        EventHandlers.setAddBtn();
     }
 }

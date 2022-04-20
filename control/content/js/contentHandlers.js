7c8d606f28a106667d5d5fc6760dd2f3eec49192
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-20 15:59:41 +0300

    change type of file content to be class

--- control/content/js/contentHandlers.js
+++ control/content/js/contentHandlers.js
@@ -1,34 +1,34 @@
 import Item from "../../../widget/common/entities/Item.js";
 import Items from "../../../widget/common/repository/Items.js";
 
-export const contentHandlers = {
-    loadItems: async(page, pageSize)=>{
+export class ContentHandlers {
+    static loadItems = async(page, pageSize)=>{
         let res = await Items.search({page, pageSize});
         if(res.length>0){
             return res
         }else{
             return []
         }
-    },
-    searchItems: async (search) => {
+    }
+    static searchItems = async (search) => {
         let res = await Items.search({search});
         if(res.length>0){
             return res
         }else{
             return []
         }
-    },
-    addItem: async(item)=>{
+    }
+    static addItem = async(item)=>{
         let res = await Items.insert(item)
         console.log("added item result -=>", res);
         return res
-    },
-    deactiveItem: async(idx, item)=>{
+    }
+    static deactiveItem = async(idx, item)=>{
         item.isActive = 0;
         let res = await Items.delete(idx, item)
         return res
-    },
-    deleteItem: async(idx)=>{
+    }
+    static deleteItem = async(idx)=>{
         let res = await Items.forceDelete(idx)
         return res
     }

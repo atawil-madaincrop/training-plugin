765bf45201a950ab711bd815b472d75e6e54cfca
Abed Tawil <aboditawil@gmail.com>
2022-04-21 18:10:56 +0300

    update loading method and add on input update callback for language helper

--- control/language/language.js
+++ control/language/language.js
@@ -1,11 +1,30 @@
-let languageHelper;
+import Language from "../../widget/common/entities/Language.js";
+import LanguageController from "./language.controller.js";
 
-const initLanguageHelper = () => {
-    languageHelper = new LanguageHelper("#language", languageConfig);
+let language, languageHelper;
+
+const initLanguageHelper = async () => {
+    const onInputUpdate = async (key, value) => {
+        language[key] = value;
+        await LanguageController.saveLanguage(language);
+    }
+
+    languageHelper = new LanguageHelper("#language", languageConfig, language, onInputUpdate);
     languageHelper.init();
 }
 
-const init = () => {
+const load = async () => {
+    const promises = [
+        LanguageController.getLanguage(),
+    ];
+
+    await Promise.all(promises).then((values) => {
+        language = new Language(values[0]?.data);
+    });
+}
+
+const init = async () => {
+    await load();
     initLanguageHelper();
 }
 

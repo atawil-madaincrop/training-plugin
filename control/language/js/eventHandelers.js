b46070ef6e7809d76913d2f886b357ec49dd253e
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-21 22:18:23 +0300

    finish language page

--- control/language/js/eventHandelers.js
+++ control/language/js/eventHandelers.js
@@ -1,23 +1,49 @@
 
 import { pointers } from "./pointers.js";
-import Language from "../../../widget/common/entities/Language.js";
 import { LanguageManagement } from "./languageManagement.js";
+import Language from "../../../widget/common/entities/Language.js";
 
-export class Handlers{
-    static languageState = new Language()
 
-    static setDefaultnput = () => {
-        pointers.search.defaultValue = Handlers.languageState.search || '';
-        pointers.sortAscend.defaultValue = Handlers.languageState.sortAscending || '';
-        pointers.sortDescend.defaultValue = Handlers.languageState.sortDescending || '';
+
+export class Handlers {
+    static languageState = new Language();
+
+    delay = 1000;
+    timer;
+
+    static setDefaultInput = () => {
+        pointers.search.defaultValue = this.languageState.search || '';
+        pointers.sortAscend.defaultValue = this.languageState.sortAscending || '';
+        pointers.sortDescend.defaultValue = this.languageState.sortDescending || '';
     }
 
     static loadLanguage = async () => {
         let res = await LanguageManagement.getData();
-        if(res){
-            Handlers.languageState = res;
-            Handlers.setDefaultnput();
+        if (res) {
+            this.languageState = res;
+            this.setDefaultInput();
         }
     }
-    
+
+    static saveLanguage = (e) => {
+        clearTimeout(this.timer);
+        this.timer = setTimeout(async x => {
+            await LanguageManagement.saveData(this.languageState);
+        }, this.delay, e)
+    }
+
+    static handelInputSearch = (e) => {
+        this.languageState.search = e.target.value;
+        this.saveLanguage(e);
+    }
+
+    static handelInputAscend = (e) => {
+        this.languageState.sortAscending = e.target.value;
+        this.saveLanguage(e);
+    }
+
+    static handelInputDescend = (e) => {
+        this.languageState.sortDescending = e.target.value;
+        this.saveLanguage(e);
+    }
 }
--- control/language/language.js
+++ control/language/language.js
@@ -1,8 +1,14 @@
 
 import { Handlers } from "./js/eventHandelers.js"
+import { pointers } from "./js/pointers.js";
+
 
 const init = async() => {
     await Handlers.loadLanguage()
+    
+    pointers.search.addEventListener('input', Handlers.handelInputSearch)
+    pointers.sortAscend.addEventListener('input', Handlers.handelInputAscend)
+    pointers.sortDescend.addEventListener('input', Handlers.handelInputDescend)
 }
 
 init();

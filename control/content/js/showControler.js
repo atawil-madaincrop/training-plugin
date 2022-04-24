d405da1f91436332f6e5e6078c4e480730444f08
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-24 19:10:11 +0300

    replace calling class methods to be in the best practice

--- control/content/js/showControler.js
+++ control/content/js/showControler.js
@@ -21,21 +21,21 @@ export class ShowControler {
         pointers.printLoading.style.display = "flex";
     }
     static printItems = () => {
-        ShowControler.loading();
-        if (ShowControler.mySateArr.length > 0) {
+        this.loading();
+        if (this.mySateArr.length > 0) {
             pointers.itemsListTable.innerHTML = "";
             pointers.printTable.style.display = "table";
             pointers.printWhenEmpty.style.display = "none";
             pointers.printLoading.style.display = "none";
 
-            ShowControler.mySateArr.forEach(ShowControler.printAllItemsInTable);
+            this.mySateArr.forEach(this.printAllItemsInTable);
         } else {
             pointers.printTable.style.display = "none";
             pointers.printWhenEmpty.style.display = "flex";
             pointers.printLoading.style.display = "none";
         }
     }
-    static printAllItemsInTable(itemElement, index) {
+    static printAllItemsInTable = (itemElement, index) => {
         let myDate = new Date(itemElement.data.createdOn).toDateString().split(" ");
         let myDateToPrint = `${myDate[1]} ${myDate[2]}, ${myDate[3]}`
 
@@ -61,22 +61,22 @@ export class ShowControler {
                 </span>
             </td>
         `
-        
+
         pointers.itemsListTable.appendChild(itemRow);
 
         let deleteBtn = document.getElementById(`deleteItemBtn-${index}`);
-        deleteBtn.addEventListener("click", () => ShowControler.deleteRow(index, itemRow, itemElement))
+        deleteBtn.addEventListener("click", () => this.deleteRow(index, itemRow, itemElement))
 
         let editTitle = document.getElementById(`titleToChange-${index}`);
-        editTitle.addEventListener("click", () => ShowControler.editRow(itemElement, index));
+        editTitle.addEventListener("click", () => this.editRow(itemElement, index));
         let editBtn = document.getElementById(`editItemBtn-${index}`);
-        editBtn.addEventListener("click", () => ShowControler.editRow(itemElement, index));
+        editBtn.addEventListener("click", () => this.editRow(itemElement, index));
     }
-    static sortData() {
-        pointers.sortSpan.className = `icon ${ShowControler.sortType}`
-        if (ShowControler.sortType == "icon-chevron-down") {
-            ShowControler.sortType = "icon-chevron-up";
-            ShowControler.mySateArr.sort(function (a, b) {
+    static sortData = () => {
+        pointers.sortSpan.className = `icon ${this.sortType}`
+        if (this.sortType == "icon-chevron-down") {
+            this.sortType = "icon-chevron-up";
+            this.mySateArr.sort(function (a, b) {
                 if (a.data.title?.toLowerCase() < b.data.title?.toLowerCase()) {
                     return -1;
                 }
@@ -84,9 +84,9 @@ export class ShowControler {
                     return 1;
                 }
             });
-        } else if (ShowControler.sortType == "icon-chevron-up") {
-            ShowControler.sortType = "icon-chevron-down";
-            ShowControler.mySateArr.sort(function (a, b) {
+        } else if (this.sortType == "icon-chevron-up") {
+            this.sortType = "icon-chevron-down";
+            this.mySateArr.sort(function (a, b) {
                 if (a.data.title?.toLowerCase() < b.data.title?.toLowerCase()) {
                     return 1;
                 }
@@ -95,65 +95,65 @@ export class ShowControler {
                 }
             });
         }
-        ShowControler.printItems();
+        this.printItems();
     }
     // Modal to add and edit data
-    static showAddModal(type) {
+    static showAddModal = (type) => {
         if (type) {
-            ShowControler.newItem = new Item();
+            this.newItem = new Item();
             pointers.itemsPageDiv.style.display = "none";
             formPage.style.display = "block";
         } else {
-            ShowControler.emptyData();
+            this.emptyData();
 
-            ShowControler.newItem = null;
+            this.newItem = null;
             pointers.itemsPageDiv.style.display = "block";
             formPage.style.display = "none";
-            ShowControler.typeOfHandelForm = "add";
-            ShowControler.itemForEdit = {};
+            this.typeOfHandelForm = "add";
+            this.itemForEdit = {};
         }
     }
-    static deleteRow(idx, itemRow, item) {
-        
+    static deleteRow = (idx, itemRow, item) => {
+
         buildfire.dialog.confirm(
             {
                 title: "Delete Item",
                 message: `Are you sure you want to Delete ${item.data.title} tem`,
             },
             async (err, isConfirmed) => {
-              if (err) console.error(err);
-          
-              if (isConfirmed) {
-                //Go back
-                itemRow.style.display = "none";
-                ShowControler.mySateArr.splice(idx, 1);
-                ShowControler.printItems();
-                await ContentHandlers.deactiveItem(item.id, item);
-              } else {
-                //Prevent action
-              }
+                if (err) console.error(err);
+
+                if (isConfirmed) {
+                    //Go back
+                    itemRow.style.display = "none";
+                    this.mySateArr.splice(idx, 1);
+                    this.printItems();
+                    await ContentHandlers.deactiveItem(item.id, item);
+                } else {
+                    //Prevent action
+                }
             }
-          );
+        );
     }
-    static editRow(itemElement, index) {
-        ShowControler.typeOfHandelForm = "edit";
-        ShowControler.itemForEdit = { itemElement, index };
-        ShowControler.showAddModal(true);
-        ShowControler.addDataToEdit();
+    static editRow = (itemElement, index) => {
+        this.typeOfHandelForm = "edit";
+        this.itemForEdit = { itemElement, index };
+        this.showAddModal(true);
+        this.addDataToEdit();
     }
-    static emptyData() {
+    static emptyData = () => {
         pointers.title.value = null;
         pointers.subTitle.value = null;
-        ShowControler.image.clear();
-        ShowControler.coverImage.clear();
+        this.image.clear();
+        this.coverImage.clear();
         tinymce.activeEditor.setContent('');
     }
-    static addDataToEdit() {
-        pointers.title.value = ShowControler.itemForEdit.itemElement.data.title;
-        pointers.subTitle.value = ShowControler.itemForEdit.itemElement.data.subtitle;
-        ShowControler.image.loadbackground(ShowControler.itemForEdit.itemElement.data.image);
-        ShowControler.coverImage.loadbackground(ShowControler.itemForEdit.itemElement.data.coverImage);
-        ShowControler.newItem = ShowControler.itemForEdit.itemElement.data;
-        tinymce.activeEditor.setContent(ShowControler.itemForEdit.itemElement.data.description || '');
+    static addDataToEdit = () => {
+        pointers.title.value = this.itemForEdit.itemElement.data.title;
+        pointers.subTitle.value = this.itemForEdit.itemElement.data.subtitle;
+        this.image.loadbackground(this.itemForEdit.itemElement.data.image);
+        this.coverImage.loadbackground(this.itemForEdit.itemElement.data.coverImage);
+        this.newItem = this.itemForEdit.itemElement.data;
+        tinymce.activeEditor.setContent(this.itemForEdit.itemElement.data.description || '');
     }
 }

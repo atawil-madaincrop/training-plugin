c3ee293bebed4403ced6d40b21e9f2be3764fbf1
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-27 01:24:06 +0300

    move loading function to event handlers file

--- widget/widget.js
+++ widget/widget.js
@@ -1,19 +1,15 @@
 import {IntroductionBuilder} from "./js/introduction/introductionBuilder.js";
 import { ContentBuilder } from "./js/content/contentBuilder.js";
-import { pointers } from "./js/pointers.js";
 import { LanguageBuilder } from "./js/language/languageBuilder.js";
 import { EventHandlers } from "./js/eventHandlers.js";
 
 
-const setLoading = (type) =>{
-    pointers.loadingWidget.style.display = `${type}`;
-    pointers.loadingItem.style.display = `${type}`;
-}
+
 const init = async() => {
-    setLoading('block');
-    IntroductionBuilder.init();
-    await ContentBuilder.loadItems();
-    setLoading('none');
+    EventHandlers.setLoading('block');
+    IntroductionBuilder.init_Introduction();
+    await ContentBuilder.init_Content();
+    EventHandlers.setLoading('none');
 
     EventHandlers.init_Events();
     LanguageBuilder.init_Language();

5b173dba908899b9824aab27cb190b0c5ef82708
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-27 00:07:57 +0300

    add new functionalities to manage our work and sort items

--- widget/js/content/contentBuilder.js
+++ widget/js/content/contentBuilder.js
@@ -1,31 +1,36 @@
 import { ContentManagement } from "./contentManagement.js";
+import { LanguageBuilder } from "../language/languageBuilder.js";
 import { pointers } from "../pointers.js";
 
 export class ContentBuilder {
     static itemsRendered = [];
+    static allItemsSorted = [];
     static page = 0;
     static pageSize = 10;
 
     static loadItems = async () => {
-        let res = await ContentManagement.loadItems(this.page, this.pageSize);
+        let res = await ContentManagement.loadItems(this.page, this.pageSize, "");
         this.itemsRendered = res;
-        
+
         pointers.contentItems.innerHTML = "";
+        this.allItemsSorted = [];
         this.printItems(this.itemsRendered);
     }
 
     static printItems = (addItemsArr) => {
         addItemsArr.forEach(this.print_Item_By_Item);
         this.createLoadMoreContainer(addItemsArr.length);
+
+        this.allItemsSorted = [...addItemsArr, ...this.allItemsSorted]
     }
 
-    static print_Item_By_Item = (item,index) => {
+    static print_Item_By_Item = (item, index) => {
         let row = document.createElement("div");
         row.className = "rowItem"
         row.setAttribute("id", `item-${index}`)
         row.innerHTML = `
             <section class="leftSection">
-                <img src="${item.data.image || "../media/imagePlaceHolder.png"}" alt="item image" >
+                <img src="${item.data.image || "./js/media/imagePlaceHolder.png"}" alt="item image" >
             </section>
             <section class="rightSection">
                 <span class="titleSpan">${item.data.title}</span>
@@ -38,7 +43,7 @@ export class ContentBuilder {
     static createLoadMoreContainer = (length) => {
         let loadMore = document.createElement("div");
         loadMore.setAttribute("id", "loadMoreDiv");
-        if(length >= this.pageSize){
+        if (length >= this.pageSize) {
             loadMore.innerHTML = `
             <button id="loadNewItems">
                 Load More Items ...
@@ -47,7 +52,7 @@ export class ContentBuilder {
             pointers.contentItems.appendChild(loadMore)
             let loadNewItems = document.getElementById("loadNewItems");
             loadNewItems.addEventListener("click", this.loadMoreManager);
-        }else{
+        } else {
             loadMore.innerHTML = `
             <span>
                 No More Items ...
@@ -57,15 +62,46 @@ export class ContentBuilder {
         }
     }
 
-    static loadMoreManager = async() => {
-        console.log("clicked -----");
+    static loadMoreManager = async () => {
         let loadMoreDiv = document.getElementById("loadMoreDiv");
         loadMoreDiv.remove();
 
-        this.page = this.pageSize;
-        this.pageSize += 10;
-        let res = await ContentManagement.loadItems(this.page, this.pageSize);
+        this.page += 1;
+        let res;
+        if (pointers.searchInput.value.length > 0) {
+            res = await ContentManagement.loadItems(this.page, this.pageSize, pointers.searchInput.value);
+        } else {
+            res = await ContentManagement.loadItems(this.page, this.pageSize, "");
+        }
         this.printItems(res);
     }
 
+    static getSearchData = async (value) => {
+        this.page = 0;
+        let res = await ContentManagement.loadItems(this.page, this.pageSize, value);
+        if (LanguageBuilder.selectedSort) {
+            console.log("here we should sort the array");
+        }
+        this.allItemsSorted = [];
+        this.printItems(res);
+    }
+
+    static sortItems = () => {
+        pointers.contentItems.innerHTML = "";
+        let sortedArr = [];
+        if (LanguageBuilder.selectedSort == "descending") {
+            this.allItemsSorted.sort((a, b) => {
+                if (a.data.title?.toUpperCase() < b.data.title?.toUpperCase()) return 1
+                else return -1
+            })
+        } else if (LanguageBuilder.selectedSort == "ascending") {
+            this.allItemsSorted.sort((a, b) => {
+                if (a.data.title?.toUpperCase() > b.data.title?.toUpperCase()) return 1
+                else return -1
+            })
+        }
+        sortedArr = this.allItemsSorted;
+        this.allItemsSorted = [];
+        this.printItems(sortedArr);
+    }
 }

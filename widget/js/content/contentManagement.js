146436941911ba6588ad2dfc366d7ddf03309c9b
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-27 00:08:43 +0300

    remove search function and update on load function to do the same needed management

--- widget/js/content/contentManagement.js
+++ widget/js/content/contentManagement.js
@@ -3,16 +3,8 @@ import Items from "../../common/repository/Items.js";
 export class ContentManagement {
     // manage data cycle in datastore
     // save, delete, edit and get items from datastore 
-    static loadItems = async(page, pageSize)=>{
-        let res = await Items.search({page, pageSize});
-        if(res.length>0){
-            return res;
-        }else{
-            return [];
-        }
-    }
-    static searchItems = async (search) => {
-        let res = await Items.search({search});
+    static loadItems = async(page, pageSize, search)=>{
+        let res = await Items.search({page, pageSize, search});
         if(res.length>0){
             return res;
         }else{

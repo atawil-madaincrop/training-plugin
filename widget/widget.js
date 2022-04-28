6a6aaa95ef2f3db0f45a1c5bdd67c38065ffe343
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-04-29 00:45:52 +0300

    add buildfire back button

--- widget/widget.js
+++ widget/widget.js
@@ -4,21 +4,25 @@ import { LanguageBuilder } from "./js/language/languageBuilder.js";
 import { EventHandlers } from "./js/eventHandlers.js";
 
 
-const handlersCollection = async() => {
+const handlersCollection = async () => {
     EventHandlers.setLoading('block');
     EventHandlers.init_Events();
 
     LanguageBuilder.init_Language();
     await ContentBuilder.init_Content();
-    
+
     IntroductionBuilder.init_Introduction();
     EventHandlers.setLoading('none');
 }
+
+
 
 const init = async () => {
     handlersCollection();
 
     buildfire.datastore.onUpdate(handlersCollection);
+    buildfire.navigation.onBackButtonClick = () => { console.log("No back avilable..."); }
+
 }
 
 init();

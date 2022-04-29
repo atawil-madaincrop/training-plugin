08957c1b56bc0d12f4c55b75c993f81eee621227
Abed Tawil <aboditawil@gmail.com>
2022-04-29 17:13:16 +0300

    add sort to subtitle and created on in content

--- control/content/js/searchTableConfig.js
+++ control/content/js/searchTableConfig.js
@@ -7,7 +7,7 @@ const searchTableConfig = {
 	columns: [
 		{ header: "", data: "${data.image || '../../widget/common/media/images/image-placeholder.png'}", type: "image", width: "40px", classes: ["image-column"] },
 		{ header: "Title", data: "${data.title || '-'}", type: "command", command: "open-item-detials", width: "100px", sortBy: "title", classes: ["command-column"] },
-		{ header: "Subtitle", data: "${data.subtitle || '-'}", type: "string", width: "100px" },
-		{ header: "Date Of Creation", data: "${ new Date(data.createdOn).toLocaleDateString('en-US', dateOptions)  }", type: "date", width: "100px" },
+		{ header: "Subtitle", data: "${data.subtitle || '-'}", type: "string", width: "100px", sortBy: "subtitle" },
+		{ header: "Date Of Creation", data: "${ new Date(data.createdOn).toLocaleDateString('en-US', dateOptions)  }", type: "date", width: "100px", sortBy: "createdOn", },
 	],
 };

923fa0e07f5462253d55102776774f5eec592334
AlaaN-Smadi <alaasmadi1010@gmail.com>
2022-05-09 19:26:08 +0300

    remove undefind state

--- widget/js/introduction/introductionBuilder-handel3.js
+++ widget/js/introduction/introductionBuilder-handel3.js
@@ -5,8 +5,7 @@ class IntroductionBuilder {
     static appendUpdatedData = async () => {
         myIntroduction = await introductionManagement.load();
         viewer.loadItems(myIntroduction.imageCarousel);
-        pointers.introductionDesc.innerHTML = myIntroduction.description
-
+        pointers.introductionDesc.innerHTML = myIntroduction.description || ''
         if((myIntroduction?.imageCarousel?.length > 0) || (myIntroduction.description?.length > 0)){
             pointers.emptyPage.style.display = "none";
 
@@ -14,12 +13,10 @@ class IntroductionBuilder {
         }
         return "block";
     }
-
     static initComponents = () => {
         viewer = new buildfire.components.carousel.view(".carousel");
         return this.appendUpdatedData();
     }
-
     static init_Introduction = () => {
         return this.initComponents();
     }
